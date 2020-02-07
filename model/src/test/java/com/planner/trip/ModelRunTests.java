62f0b0cfa590dff8cd76c3d2fa2c9c57f398a947
shson <tjdgus4722@naver.com>
2020-02-07 23:36:28 +0900

    user repository 테스트 코드 작성

---
+++ model/src/test/java/com/planner/trip/ModelRunTests.java
@@ -0,0 +1,9 @@
+package com.planner.trip;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModelRunTests {
+    public void contextLoads() {
+    }
+}
